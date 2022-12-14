9abecf0f34c3be2a87d50283beac7b97a5e0f22c
jasondoss <jason.j.doss@gmail.com>
2022-12-14 15:53:36 -0800

    added to server.js and got running

--- server.js
+++ server.js
@@ -5,9 +5,54 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const { notes } = require('./db/db');
+const { notes } = require('./Develop/db/db.json');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+const generateUniqueID = require('generate-unique-id');
+
+// Create note
+function createNote(body, notesArray) {
+  const note = body;
+  notesArray.push(note);
+  fs.writeFileSync(
+      path.join(__dirname, './Develop/db/db.json'),
+      JSON.stringify({ notes: notesArray }, null, 2)
+    );
+  return note;
+  };  
+
+  // routes
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+  });
+
+  app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+  req.body.id = generateUniqueID();
+  const note = createNote(req.body, notes);
+  res.json(note);
+});
+
+//bonus
+app.delete('/api/notes/:id', (req, res) => {
+  const { id } = req.params;
+
+  const delNote = notes.findIndex(note => note.id ==id);
+
+  notes.splice(delNote, 1);
+  return res.send();
+});
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+
+  
